ce6a95c9ca0af373e8a7d7ef405061f2c7c85b6b
Blair <bgriffit@oberlin.edu>
2019-06-17 10:40:00 -0400

    changed HomeController to ReviewController to match new naming conventions

---
+++ Reviews/Controllers/ReviewController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reviews.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reviews.Controllers
+{
+    public class ReviewController : Controller
+    {
+        public ViewResult Index()
+        {
+            ReviewRepository revrepos = new ReviewRepository();
+            var model = revrepos.GetAll();
+            return View(model);
+        }
+    }
+}
