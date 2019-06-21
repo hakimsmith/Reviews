fb5d407e3f211305111d73fa5ad24f0526ce6d33
mega24cde <megan.denman@gmail.com>
2019-06-21 12:27:04 -0400

    added genreID in movie class and in site context. finished adding reviews and genres in site context.

--- Reviews/Model/Movie.cs
+++ Reviews/Model/Movie.cs
@@ -8,9 +8,11 @@ namespace Reviews.Model
     public class Movie
     {
         public int MovieId { get; set; }
+        public int GenreId { get; set; }
         public string Title { get; set; }
         public string ImageUrl { get; set; }
         public string Description { get; set; }
+        
 
         public Genre Genre { get; set; }
 
@@ -19,9 +21,10 @@ namespace Reviews.Model
 
 
 
-        public Movie(int movieId, string title, string imageUrl, string description)
+        public Movie(int movieId, int genreId, string title, string imageUrl, string description)
         {
-            MovieId = MovieId;
+            MovieId = movieId;
+            GenreId = genreId;
             Title = title;
             ImageUrl = imageUrl;
             Description = description;           
