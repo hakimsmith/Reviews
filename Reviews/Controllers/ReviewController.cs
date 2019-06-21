687499df7120b5a4c48274c55520422aa44987af
mega24cde <megan.denman@gmail.com>
2019-06-21 10:57:59 -0400

    controller updates

---
+++ Reviews/Controllers/ReviewController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reviews.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reviews.Controllers 
+{
+    public class ReviewController
+    {
+        public SiteContext Db = new SiteContext();
+
+            
+        
+            public ViewResult GetById(int id)
+            {
+                return Db.(c => c.HorrorReviewsId == id);
+            }
+        
+    }
+}
