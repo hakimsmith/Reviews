14b8691a21f60daeeb10a71dfea8498151cbb901
Hakim Smith <hakim.smith19@yahoo.com>
2019-06-21 15:01:53 -0400

    AddedGenreHTMLFiles

--- Reviews/Controllers/GenreController.cs
+++ Reviews/Controllers/GenreController.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Reviews.Model;
+using Reviews.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Reviews.Controllers
 {
-    public class GenreController
+    public class GenreController : Controller
     {
+        IRepository<Genre> genrerepos;
+        public GenreController(IRepository<Genre> genrerepos)
+        {
+            this.genrerepos = genrerepos;
+        }
+
+        public ViewResult AllGenres()
+        {
+            var model = genrerepos.GetAll();
+            return View(model);
+        }
+        public ActionResult SingleGenres(int id)
+        {
+            var model = genrerepos.GetById(id);
+            return View(model);
+        }
+
     }
 }
