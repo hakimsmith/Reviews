26e8e1361f30bcad5febbcb413d717b247d8218f
Blair <bgriffit@oberlin.edu>
2019-06-27 11:39:45 -0400

    changed images

---
+++ Reviews/Migrations/20190627153632_ChangedImagePathForGenre3.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Reviews.Migrations
+{
+    public partial class ChangedImagePathForGenre3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Genres",
+                keyColumn: "GenreId",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "Supernatural.png");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Genres",
+                keyColumn: "GenreId",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "Supernatural.jpg");
+        }
+    }
+}
