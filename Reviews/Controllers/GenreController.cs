d2434ab8187cfe41ab890aeb2dd49daceaa02b5c
Sergio Rodriguez <rsergio98@yahoo.com>
2019-06-27 10:18:43 -0400

    added comments in GenreController and MovieController

--- Reviews/Controllers/GenreController.cs
+++ Reviews/Controllers/GenreController.cs
@@ -11,16 +11,20 @@ namespace Reviews.Controllers
     public class GenreController : Controller
     {
         IRepository<Genre> genrerepos;
+
         public GenreController(IRepository<Genre> genrerepos)
         {
             this.genrerepos = genrerepos;
         }
 
+        //Takes user to the main landing page named AllGenres, displays genres.
         public ViewResult AllGenres()
         {
             var model = genrerepos.GetAll();
             return View(model);
         }
+
+        //Takes user to the Single Genre page which displays the movies within the selected genre.
         public ActionResult SingleGenre(int id)
         {
             var model = genrerepos.GetById(id);
